af79278132fb1423d17d8080606ea791a0ff401b
Moosa <hello@moosashah.com>
2025-09-27 19:47:43 +0100

    remove short ttl

--- apps/auth/index.ts
+++ apps/auth/index.ts
@@ -1,64 +1,65 @@
-import { Hono } from "hono"
-import { issuer } from "@openauthjs/openauth"
-import { object, string, safeParse } from "valibot"
-import { GoogleProvider } from "@openauthjs/openauth/provider/google"
-import { DynamoStorage } from "@openauthjs/openauth/storage/dynamo"
-import { Jwt } from "hono/utils/jwt";
+import { Hono } from 'hono';
+import { issuer } from '@openauthjs/openauth';
+import { object, string, safeParse } from 'valibot';
+import { GoogleProvider } from '@openauthjs/openauth/provider/google';
+import { DynamoStorage } from '@openauthjs/openauth/storage/dynamo';
+import { Jwt } from 'hono/utils/jwt';
 
 const userInfoSchema = object({
-    sub: string(),
-    email: string(),
-    name: string(),
-    picture: string(),
-})
+  sub: string(),
+  email: string(),
+  name: string(),
+  picture: string(),
+});
 
 const authIssuer = issuer({
-    subjects: {
-        user: object({
-            id: string(),
-            email: string(),
-            name: string(),
-            picture: string(),
-        }),
-    },
-    ttl: {
-        access: 60 * 5, // 5 mins
-        refresh: 60 * 60 * 24 * 365, // 1 year
-    },
-    storage: DynamoStorage({
-        table: "guestbook-auth",
+  subjects: {
+    user: object({
+      id: string(),
+      email: string(),
+      name: string(),
+      picture: string(),
     }),
-    // Remove after setting custom domain
-    allow: async () => true,
-    providers: {
-        google: GoogleProvider({
-            clientID: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-            scopes: ["email", "profile"]
-        })
-    },
-    success: async (ctx, value) => {
-        if (value.provider === "google") {
-            const result = safeParse(userInfoSchema, Jwt.decode(value.tokenset.raw.id_token).payload)
+  },
+  //TODO: add shorter ttl for access token.. currently its set for a month and refresh token for a year
+  // refreshing access token is broken so not setting it for now
+  storage: DynamoStorage({
+    table: 'guestbook-auth',
+  }),
+  // Remove after setting custom domain
+  allow: async () => true,
+  providers: {
+    google: GoogleProvider({
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      scopes: ['email', 'profile'],
+    }),
+  },
+  success: async (ctx, value) => {
+    if (value.provider === 'google') {
+      const result = safeParse(
+        userInfoSchema,
+        Jwt.decode(value.tokenset.raw.id_token).payload
+      );
 
-            if (!result.success) {
-                throw new Error("Failed to decode user information")
-            }
+      if (!result.success) {
+        throw new Error('Failed to decode user information');
+      }
 
-            return ctx.subject("user", {
-                id: result.output.sub, // Google's user ID
-                email: result.output.email,
-                name: result.output.name,
-                picture: result.output.picture,
-            })
-        }
-        throw new Error("Invalid provider")
-    },
-})
+      return ctx.subject('user', {
+        id: result.output.sub, // Google's user ID
+        email: result.output.email,
+        name: result.output.name,
+        picture: result.output.picture,
+      });
+    }
+    throw new Error('Invalid provider');
+  },
+});
 
-const app = new Hono()
+const app = new Hono();
 
 // Mount the auth issuer on all routes
-app.all("*", (c) => authIssuer.fetch(c.req.raw))
+app.all('*', c => authIssuer.fetch(c.req.raw));
 
-export default app
+export default app;
